67ced62da398631152103bde25d8dc9cc5198530
Victoria Beasley <iesnoth91@gmail.com>
2023-02-04 10:52:15 -0600

    cookies

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,11 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
+//This component provides a React context that shares the logged-in user
+// across the application, which makes it a natural place for us
+// to check for an already-logged-in user on page load.
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -20,22 +20,23 @@ function LoginForm() {
     //the user entered, so that our back end can use it to lookup.
     async function handleSubmit(e) {
         e.preventDefault()
-       const response = await fetch(`http://localhost:5000/authentication`,{
-        method: 'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(credentials)
-       })
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
 
-       const data = await response.json()
+        const data = await response.json()
 
-       if (response.status === 200){
-        setCurrentUser(data.user)
-        history.push('/')
-       } else{
-        setErrorMessage(data.message)
-       }
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push('/')
+        } else {
+            setErrorMessage(data.message)
+        }
 
     }
 
