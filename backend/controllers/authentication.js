859b93daec0ab2ad4d8aa1acb31e02ada584669e
Victoria Beasley <iesnoth91@gmail.com>
2023-02-04 12:43:33 -0600

    starting jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
@@ -14,8 +15,21 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password.`
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
     }
+})
+
+router.get('/profile', async (req, res) => {
+    // try{
+    //     let user = await User.findOne({
+    //         where: {
+    //             userId:''
+    //         }
+    //     })
+    // } catch{
+    //     res.json(null)
+    // }
 })
 
 module.exports = router
